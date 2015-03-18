87c6a210be7c4eedb8f907b357fe52bf91da49c0
morkt <muvluv.ru@gmail.com>
2015-03-18 05:44:21 +0400

    implemented ISA archives.

---
+++ ArcFormats/ArcISA.cs
@@ -0,0 +1,67 @@
+//! \file       ArcISA.cs
+//! \date       Tue Mar 17 09:21:03 2015
+//! \brief      ISM engine resource archive.
+//
+// Copyright (C) 2015 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using GameRes.Utility;
+
+namespace GameRes.Formats.ISM
+{
+    [Export(typeof(ArchiveFormat))]
+    public class IsaOpener : ArchiveFormat
+    {
+        public override string         Tag { get { return "ISA"; } }
+        public override string Description { get { return "ISM engine resource archive"; } }
+        public override uint     Signature { get { return 0x204d5349; } } // 'ISM '
+        public override bool  IsHierarchic { get { return false; } }
+        public override bool     CanCreate { get { return false; } }
+
+        public override ArcFile TryOpen (ArcView file)
+        {
+            if (!file.View.AsciiEqual (4, "ARCHIVED"))
+                return null;
+            int count = file.View.ReadInt16 (0x0c);
+            if (count <= 0)
+                return null;
+            file.View.Reserve (0x10, (uint)count*0x20u);
+            int index_offset = 0x10;
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
+            {
+                var name = file.View.ReadString (index_offset, 0x0c);
+                var entry = FormatCatalog.Instance.CreateEntry (name);
+                entry.Offset = file.View.ReadUInt32 (index_offset+0x10);
+                entry.Size = file.View.ReadUInt32 (index_offset+0x14);
+                if (!entry.CheckPlacement (file.MaxOffset))
+                    return null;
+                dir.Add (entry);
+                index_offset += 0x20;
+            }
+            return new ArcFile (file, this, dir);
+        }
+    }
+}
