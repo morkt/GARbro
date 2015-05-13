53b707778689e7744ae3fe787bbbc65c799002f5
morkt <muvluv.ru@gmail.com>
2015-05-13 23:26:30 +0400

    assembly version update.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.4.14")]
-[assembly: AssemblyFileVersion ("1.0.4.14")]
+[assembly: AssemblyVersion ("1.0.4.15")]
+[assembly: AssemblyFileVersion ("1.0.4.15")]
