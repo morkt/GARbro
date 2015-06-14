227eae0dbe9486653b9a31eafd5975a0d7b1bc2b
morkt <muvluv.ru@gmail.com>
2015-06-14 04:10:00 +0400

    convert OGV format into Ogg propery.

--- ArcFormats/AudioOGV.cs
+++ ArcFormats/AudioOGV.cs
@@ -25,6 +25,7 @@
 
 using System.ComponentModel.Composition;
 using System.IO;
+using GameRes.Utility;
 
 namespace GameRes.Formats.ShiinaRio
 {
@@ -37,21 +38,29 @@ namespace GameRes.Formats.ShiinaRio
         
         public override SoundInput TryOpen (Stream file)
         {
-            var input = file as MemoryStream;
-            if (null == input || !input.CanWrite)
+            file.Position = 0xc;
+            var header = new byte[8];
+            if (8 != file.Read (header, 0, 8))
+                return null;
+            if (!Binary.AsciiEqual (header, 0, "fmt "))
+                return null;
+            uint offset = LittleEndian.ToUInt32 (header, 4);
+            file.Seek (offset, SeekOrigin.Current);
+            if (8 != file.Read (header, 0, 8))
+                return null;
+            if (!Binary.AsciiEqual (header, 0, "data"))
+                return null;
+
+            var input = new StreamRegion (file, file.Position);
+            try
             {
-                input = new MemoryStream();
-                file.CopyTo (input);
+                return new OggInput (input);
             }
-            input.Position = 1;
-            input.WriteByte ((byte)'g');
-            input.WriteByte ((byte)'g');
-            input.WriteByte ((byte)'S');
-            input.Position = 0;
-            var ogg = new OggInput (input);
-            if (file != input)
-                file.Dispose();
-            return ogg;
+            catch
+            {
+                input.Dispose();
+                throw;
+            }
         }
     }
 }
