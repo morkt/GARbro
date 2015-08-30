47b3d6adf062a56fbdaf9e6afc3deaa1cdb688b2
morkt <muvluv.ru@gmail.com>
2015-08-31 01:22:50 +0400

    released v1.1.6

--- GameRes/Properties/AssemblyInfo.cs
+++ GameRes/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.1.7.89")]
-[assembly: AssemblyFileVersion ("1.1.7.89")]
+[assembly: AssemblyVersion ("1.1.7.90")]
+[assembly: AssemblyFileVersion ("1.1.7.90")]
