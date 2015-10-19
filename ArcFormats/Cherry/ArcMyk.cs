09f21a2fcce192484e7b9f339c7ffd6cf340082b
morkt <muvluv.ru@gmail.com>
2015-10-19 09:05:26 +0400

    CherrySoft archives variations.

---
+++ ArcFormats/Cherry/ArcMyk.cs
@@ -0,0 +1,78 @@
+//! \file       ArcMyk.cs
+//! \date       Sat Oct 17 03:45:02 2015
+//! \brief      Cherry Soft MYK archives.
+//
+// Copyright (C) 2015 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace GameRes.Formats.Cherry
+{
+    [Export(typeof(ArchiveFormat))]
+    public class DatOpener : ArchiveFormat
+    {
+        public override string         Tag { get { return "DAT/CHERRY"; } }
+        public override string Description { get { return "Cherry Soft resource archive"; } }
+        public override uint     Signature { get { return 0x304B594D; } } // 'MYK0'
+        public override bool  IsHierarchic { get { return false; } }
+        public override bool     CanCreate { get { return false; } }
+
+        public DatOpener ()
+        {
+            Extensions = new string[] { "dat" };
+            Signatures = new uint[] { 0x304B594D, 0x30454843 }; // 'MYK0', 'CHE0'
+        }
+
+        public override ArcFile TryOpen (ArcView file)
+        {
+            if (0x1A30 != file.View.ReadUInt16 (4))
+                return null;
+            int count = file.View.ReadUInt16 (8);
+            if (!IsSaneCount (count))
+                return null;
+            uint index_offset = file.View.ReadUInt32 (0xA);
+            if (index_offset >= file.MaxOffset)
+                return null;
+            uint index_size = (uint)count * 0x10u;
+            if (index_size > file.View.Reserve (index_offset, index_size))
+                return null;
+            uint base_offset = 0x10;
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
+            {
+                string name = file.View.ReadString (index_offset, 0xC);
+                if (0 == name.Length)
+                    return null;
+                var entry = FormatCatalog.Instance.Create<Entry> (name);
+                entry.Offset = base_offset;
+                entry.Size = file.View.ReadUInt32 (index_offset+0xC);
+                if (!entry.CheckPlacement (file.MaxOffset))
+                    return null;
+                dir.Add (entry);
+                base_offset += entry.Size;
+                index_offset += 0x10;
+            }
+            return new ArcFile (file, this, dir);
+        }
+    }
+}
