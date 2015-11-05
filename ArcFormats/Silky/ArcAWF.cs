ce559e7ea43dd2b17165720e32e610ecb9e412e3
morkt <muvluv.ru@gmail.com>
2015-11-06 02:38:45 +0400

    partially implemented AWF audio archives.

---
+++ ArcFormats/Silky/ArcAWF.cs
@@ -0,0 +1,79 @@
+//! \file       ArcAWF.cs
+//! \date       Thu Nov 05 01:47:53 2015
+//! \brief      Silky's audio archive.
+//
+// Copyright (C) 2015 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using GameRes.Utility;
+
+namespace GameRes.Formats.Silky
+{
+    [Export(typeof(ArchiveFormat))]
+    public class AwfOpener : ArchiveFormat
+    {
+        public override string         Tag { get { return "AWF"; } }
+        public override string Description { get { return "Silky's audio archive"; } }
+        public override uint     Signature { get { return 0; } }
+        public override bool  IsHierarchic { get { return false; } }
+        public override bool     CanCreate { get { return false; } }
+
+        public override ArcFile TryOpen (ArcView file)
+        {
+            // enforce extension to avoid false positives
+            if (!file.Name.EndsWith (".awf", StringComparison.InvariantCultureIgnoreCase))
+                return null;
+            int count = file.View.ReadInt32 (0);
+            if (!IsSaneCount (count))
+                return null;
+            uint index_offset = 4;
+            uint index_size = (uint)count*0x34;
+            if (index_size > file.View.Reserve (index_offset, index_size))
+                return null;
+
+            // rather loose criterion, haven't found anything better yet.
+            bool is_mp3 = Path.GetFileName (file.Name).Equals ("voice.awf", StringComparison.InvariantCultureIgnoreCase);
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
+            {
+                var name = file.View.ReadString (index_offset, 0x20);
+                // haven't figured out this field yet.
+//                int compression = file.View.ReadInt32 (index_offset+0x28);
+//                if (-1 == compression)
+//                    name = Path.ChangeExtension (name, "mp3");
+                if (is_mp3)
+                    name = Path.ChangeExtension (name, "mp3");
+                var entry = FormatCatalog.Instance.Create<Entry> (name);
+                entry.Offset = file.View.ReadUInt32 (index_offset+0x20);
+                entry.Size   = file.View.ReadUInt32 (index_offset+0x24);
+                if (!entry.CheckPlacement (file.MaxOffset))
+                    return null;
+                dir.Add (entry);
+                index_offset += 0x34;
+            }
+            return new ArcFile (file, this, dir);
+        }
+    }
+}
