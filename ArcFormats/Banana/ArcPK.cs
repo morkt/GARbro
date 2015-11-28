e7814ba98554736b7c2b33b8257203d9e261a4f0
morkt <muvluv.ru@gmail.com>
2015-11-28 08:12:08 +0400

    implemented PK archives and MAG images.
    "BANANA Shu-Shu" formats

---
+++ ArcFormats/Banana/ArcPK.cs
@@ -0,0 +1,97 @@
+//! \file       ArcPK.cs
+//! \date       Sat Nov 28 02:45:36 2015
+//! \brief      BANANA Shu-Shu PK archives implementation.
+//
+// Copyright (C) 2015 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using GameRes.Utility;
+using GameRes.Compression;
+
+namespace GameRes.Formats.Banana // namespace is arbitrary, actual format source is uncertain
+{
+    [Export(typeof(ArchiveFormat))]
+    public class PkOpener : ArchiveFormat
+    {
+        public override string         Tag { get { return "PK/BANANA"; } }
+        public override string Description { get { return "BANANA Shu-Shu resource archive"; } }
+        public override uint     Signature { get { return 0; } }
+        public override bool  IsHierarchic { get { return false; } }
+        public override bool     CanCreate { get { return false; } }
+
+        public PkOpener ()
+        {
+            Extensions = new string[] { "pk" };
+        }
+
+        public override ArcFile TryOpen (ArcView file)
+        {
+            int count = file.View.ReadInt32 (0);
+            if (!IsSaneCount (count) || count * 10 >= file.MaxOffset)
+                return null;
+
+            uint index_offset = 4;
+            byte[] name_buffer = new byte[0x100];
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
+            {
+                byte name_length = file.View.ReadByte (index_offset++);
+                if (0 == name_length)
+                    return null;
+                if (name_length != file.View.Read (index_offset, name_buffer, 0, name_length))
+                    return null;
+                index_offset += name_length;
+                byte key = (byte)(name_length+1);
+                for (int j = 0; j < name_length; ++j)
+                {
+                    name_buffer[j] -= key--;
+                    if (name_buffer[j] < 0x20)
+                        return null;
+                }
+                string name = Encodings.cp932.GetString (name_buffer, 0, name_length);
+
+                var entry = FormatCatalog.Instance.Create<PackedEntry> (name);
+                entry.Offset = Binary.BigEndian (file.View.ReadUInt32 (index_offset));
+                entry.Size   = Binary.BigEndian (file.View.ReadUInt32 (index_offset+4));
+                index_offset += 8;
+                if (entry.Offset < index_offset || !entry.CheckPlacement (file.MaxOffset))
+                    return null;
+                if (name.EndsWith (".scr", StringComparison.InvariantCultureIgnoreCase))
+                    entry.IsPacked = true;
+                dir.Add (entry);
+            }
+            return new ArcFile (file, this, dir);
+        }
+
+        public override Stream OpenEntry (ArcFile arc, Entry entry)
+        {
+            var pent = entry as PackedEntry;
+            if (null == pent || !pent.IsPacked)
+                return base.OpenEntry (arc, entry);
+            var input = arc.File.CreateStream (entry.Offset, entry.Size);
+            return new LzssStream (input);
+        }
+    }
+}
---
+++ ArcFormats/Banana/ImageMAG.cs
@@ -0,0 +1,136 @@
+//! \file       ImageMAG.cs
+//! \date       Sat Nov 28 03:17:00 2015
+//! \brief      BANANA Shu-Shu MAG images implementation.
+//
+// Copyright (C) 2015 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using GameRes.Compression;
+using GameRes.Utility;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Windows.Media;
+
+namespace GameRes.Formats.Banana
+{
+    internal class MagMetaData : ImageMetaData
+    {
+        public uint AlphaOffset;
+        public int  BackWidth;
+        public int  BackHeight;
+    }
+
+    [Export(typeof(ImageFormat))]
+    public class MagFormat : ImageFormat
+    {
+        public override string         Tag { get { return "MAG"; } }
+        public override string Description { get { return "BANANA Shu-Shu image format"; } }
+        public override uint     Signature { get { return 0; } }
+
+        public override ImageMetaData ReadMetaData (Stream stream)
+        {
+            var header = new byte[0x24];
+            if (header.Length != stream.Read (header, 0, header.Length))
+                return null;
+            if (0 != LittleEndian.ToInt32 (header, 0x10) || 0 != LittleEndian.ToInt32 (header, 0x14))
+                return null;
+            int left = LittleEndian.ToInt32 (header, 0);
+            int top  = LittleEndian.ToInt32 (header, 4);
+            int right = LittleEndian.ToInt32 (header, 8);
+            int bottom = LittleEndian.ToInt32 (header, 0xC);
+            int back_width = LittleEndian.ToInt32 (header, 0x18);
+            int back_height = LittleEndian.ToInt32 (header, 0x1C);
+            uint alpha_channel = LittleEndian.ToUInt32 (header, 0x20);
+            int width = right - left;
+            int height = bottom - top;
+            if (left >= back_width || top >= back_height
+                || width <= 0 || width > back_width || height <= 0 || height > back_height
+                || back_width <= 0 || back_width > 0x2000 || back_height <= 0 || back_height > 0x2000)
+                return null;
+            return new MagMetaData
+            {
+                Width   = (uint)width,
+                Height  = (uint)height,
+                BPP     = alpha_channel != 0 ? 32 : 24,
+                OffsetX = left,
+                OffsetY = top,
+                AlphaOffset = alpha_channel,
+                BackWidth = back_width,
+                BackHeight = back_height,
+            };
+        }
+
+        public override ImageData Read (Stream stream, ImageMetaData info)
+        {
+            int stride = (int)info.Width * 3;
+            var pixels = new byte[stride * (int)info.Height];
+            stream.Position = 0x24;
+            using (var lz = new LzssStream (stream, LzssMode.Decompress, true))
+            {
+                if (pixels.Length != lz.Read (pixels, 0, pixels.Length))
+                    throw new InvalidFormatException();
+            }
+            int src = 0;
+            for (int i = 3; i < stride; ++i)
+            {
+                pixels[i] += pixels[src++];
+            }
+            src = 0;
+            for (int i = stride; i < pixels.Length; ++i)
+            {
+                pixels[i] += pixels[src++];
+            }
+            var meta = (MagMetaData)info;
+            if (0 == meta.AlphaOffset)
+                return ImageData.CreateFlipped (info, PixelFormats.Bgr24, null, pixels, stride);
+
+            stream.Position = 0x24 + meta.AlphaOffset;
+            var alpha = new byte[meta.BackWidth*meta.BackHeight];
+            using (var lz = new LzssStream (stream, LzssMode.Decompress, true))
+            {
+                if (alpha.Length != lz.Read (alpha, 0, alpha.Length))
+                    throw new InvalidFormatException();
+            }
+            int img_stride = (int)info.Width*4;
+            var img = new byte[img_stride * (int)info.Height];
+            int dst = 0;
+            int alpha_y = meta.BackHeight - (meta.OffsetY + (int)meta.Height);
+            for (int y = (int)meta.Height - 1; y >= 0; --y)
+            {
+                src = stride * y;
+                int src_alpha = meta.BackWidth * (alpha_y + y) + meta.OffsetX;
+                for (int i = 0; i < img_stride; i += 4)
+                {
+                    img[dst++] = pixels[src++];
+                    img[dst++] = pixels[src++];
+                    img[dst++] = pixels[src++];
+                    img[dst++] = alpha[src_alpha++];
+                }
+            }
+            return ImageData.Create (info, PixelFormats.Bgra32, null, img, img_stride);
+        }
+
+        public override void Write (Stream file, ImageData image)
+        {
+            throw new System.NotImplementedException ("MagFormat.Write not implemented");
+        }
+    }
+}
