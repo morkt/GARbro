d1e6ee4a35eb4e7d64700ce45c21dc2f22f69425
morkt <muvluv.ru@gmail.com>
2016-01-05 23:54:54 +0400

    implemented Xuse audio archives.

---
+++ ArcFormats/Xuse/ArcBIN.cs
@@ -0,0 +1,84 @@
+//! \file       ArcBIN.cs
+//! \date       Tue Jan 05 20:11:59 2016
+//! \brief      Xuse audio archive.
+//
+// Copyright (C) 2015 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+
+namespace GameRes.Formats.Xuse
+{
+    [Export(typeof(ArchiveFormat))]
+    public class BinOpener : ArchiveFormat
+    {
+        public override string         Tag { get { return "BIN/Xuse"; } }
+        public override string Description { get { return "Xuse audio archive"; } }
+        public override uint     Signature { get { return 1; } }
+        public override bool  IsHierarchic { get { return false; } }
+        public override bool     CanCreate { get { return false; } }
+
+        public BinOpener ()
+        {
+            Extensions = new string[] { "bin", "" };
+        }
+
+        public override ArcFile TryOpen (ArcView file)
+        {
+            long first_offset = file.View.ReadInt64 (8);
+            if (first_offset <= 0x14 || first_offset >= file.MaxOffset || first_offset > int.MaxValue)
+                return null;
+            var count = (int)(first_offset - 4) / 0x10;
+            if (count != 0x100)
+                return null;
+
+            string base_name = Path.GetFileNameWithoutExtension (file.Name);
+            uint index_offset = 4;
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
+            {
+                var offset = file.View.ReadInt64 (index_offset+4);
+                if (0 == offset)
+                    break;
+                if (offset < first_offset)
+                    return null;
+                string name = string.Format ("{0}#{1:D4}", base_name, i);
+                var entry = new AutoEntry (name, () => {
+                    uint signature = file.View.ReadUInt32 (offset);
+                    if (0 == signature) return null;
+                    return FormatCatalog.Instance.LookupSignature (signature).FirstOrDefault();
+                });
+                entry.Offset = offset;
+                entry.Size = file.View.ReadUInt32 (index_offset);
+                if (!entry.CheckPlacement (file.MaxOffset))
+                    return null;
+                dir.Add (entry);
+                index_offset += 0x10;
+            }
+            if (0 == dir.Count)
+                return null;
+            return new ArcFile (file, this, dir);
+        }
+    }
+}
