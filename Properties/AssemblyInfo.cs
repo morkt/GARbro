f85cf12fcdda64bbbdb7f05d52dc3ee1af2157fa
morkt <muvluv.ru@gmail.com>
2016-01-07 18:08:46 +0400

    released v1.2.12

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.2.11.910")]
-[assembly: AssemblyFileVersion ("1.2.11.910")]
+[assembly: AssemblyVersion ("1.2.12.953")]
+[assembly: AssemblyFileVersion ("1.2.12.953")]
