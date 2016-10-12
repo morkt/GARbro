92c0ed03e622eadc993186c70db6cb077cd88f71
morkt <muvluv.ru@gmail.com>
2016-10-12 11:59:48 +0400

    (EAGLS): encryption variation.

---
+++ ArcFormats/Eagls/WidgetEAGLS.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using System.Linq;
+using GameRes.Formats.Eagls;
+using GameRes.Formats.Strings;
+
+namespace GameRes.Formats.GUI
+{
+    /// <summary>
+    /// Interaction logic for WidgetEAGLS.xaml
+    /// </summary>
+    public partial class WidgetEAGLS : StackPanel
+    {
+        public WidgetEAGLS ()
+        {
+            InitializeComponent ();
+            var schemes = new string[] { arcStrings.ArcIgnoreEncryption };
+            Scheme.ItemsSource = schemes.Concat (PakOpener.KnownSchemes.Keys);
+            if (-1 == Scheme.SelectedIndex)
+                Scheme.SelectedValue = PakOpener.KnownSchemes.First().Key;
+        }
+    }
+}
