5923894106f9888c3a0a8146f0ac3183a5f8270f
morkt <muvluv.ru@gmail.com>
2017-12-19 21:13:25 +0400

    (Legacy): added PACK archives and WMK images.

---
+++ Legacy/CocktailSoft/ImageWMK.cs
@@ -0,0 +1,64 @@
+//! \file       ImageWMK.cs
+//! \date       2017 Dec 15
+//! \brief      Cocktail Soft bitmap mask format.
+//
+// Copyright (C) 2017 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Windows.Media;
+
+namespace GameRes.Formats.Cocktail
+{
+    [Export(typeof(ImageFormat))]
+    public class WmkFormat : ImageFormat
+    {
+        public override string         Tag { get { return "WMK"; } }
+        public override string Description { get { return "Cocktail Soft bitmap mask format"; } }
+        public override uint     Signature { get { return 0; } }
+
+        public override ImageMetaData ReadMetaData (IBinaryStream file)
+        {
+            uint width = file.ReadUInt32();
+            uint height = file.ReadUInt32();
+            if (width * height + 0x10 != file.Length)
+                return null;
+            return new ImageMetaData {
+                Width = width,
+                Height = height,
+                BPP = 8,
+            };
+        }
+
+        public override ImageData Read (IBinaryStream file, ImageMetaData info)
+        {
+            file.Position = 0x10;
+            var pixels = file.ReadBytes ((int)info.Width * (int)info.Height);
+            return ImageData.Create (info, PixelFormats.Gray8, null, pixels);
+        }
+
+        public override void Write (Stream file, ImageData image)
+        {
+            throw new System.NotImplementedException ("WmkFormat.Write not implemented");
+        }
+    }
+}
