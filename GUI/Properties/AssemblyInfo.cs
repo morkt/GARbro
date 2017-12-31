d6b2140304324b7ee38ad788be8f8b1bf6cec571
morkt <muvluv.ru@gmail.com>
2017-12-31 10:37:39 +0400

    released v1.4.33

--- GUI/Properties/AssemblyInfo.cs
+++ GUI/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.4.32.1939")]
-[assembly: AssemblyFileVersion ("1.4.32.1939")]
+[assembly: AssemblyVersion ("1.4.33.2080")]
+[assembly: AssemblyFileVersion ("1.4.33.2080")]
