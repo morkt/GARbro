b66893a2aa4a1eae90f6f1be8e919460517e96bf
morkt <muvluv.ru@gmail.com>
2017-12-31 09:02:15 +0400

    (PkOpener): added "pke" extension.

--- ArcFormats/UMeSoft/ArcPK.cs
+++ ArcFormats/UMeSoft/ArcPK.cs
@@ -42,7 +42,7 @@ namespace GameRes.Formats.UMeSoft
 
         public PkOpener ()
         {
-            Extensions = new string[] { "pk", "gpk", "tpk", "wpk", "mpk" };
+            Extensions = new string[] { "pk", "gpk", "tpk", "wpk", "mpk", "pke" };
         }
 
         public override ArcFile TryOpen (ArcView file)
