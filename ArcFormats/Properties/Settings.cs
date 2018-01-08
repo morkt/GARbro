2e2e447c422b676570bb9e4f2dc6e428898f6b42
morkt <muvluv.ru@gmail.com>
2018-01-08 19:56:31 +0400

    moved Settings to Properties folder.

---
+++ ArcFormats/Properties/Settings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace GameRes.Formats.Properties {
+    
+    
+    // This class allows you to handle specific events on the settings class:
+    //  The SettingChanging event is raised before a setting's value is changed.
+    //  The PropertyChanged event is raised after a setting's value is changed.
+    //  The SettingsLoaded event is raised after the setting values are loaded.
+    //  The SettingsSaving event is raised before the setting values are saved.
+    public sealed partial class Settings {
+        
+        public Settings() {
+            // // To add event handlers for saving and changing settings, uncomment the lines below:
+            //
+            // this.SettingChanging += this.SettingChangingEventHandler;
+            //
+            // this.SettingsSaving += this.SettingsSavingEventHandler;
+            //
+
+            if (null != Application.Current)
+            {
+                Application.Current.Dispatcher.BeginInvoke (DispatcherPriority.Normal, new Action(() => {
+                    Application.Current.Exit += ApplicationExitHandler;
+                }));
+            }
+        }
+
+        void ApplicationExitHandler (object sender, System.Windows.ExitEventArgs args)
+        {
+            Default.Save();
+        }
+        
+        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
+            // Add code to handle the SettingChangingEvent event here.
+        }
+        
+        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
+            // Add code to handle the SettingsSaving event here.
+        }
+    }
+}
