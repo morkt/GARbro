06a6a26a7fe9961c3cf50ef949091c786b214007
morkt <muvluv.ru@gmail.com>
2018-01-16 11:57:27 +0400

    (Legacy): implemented FPACK archives.

---
+++ Legacy/Iris/ArcFPACK.cs
@@ -0,0 +1,72 @@
+//! \file       ArcFPACK.cs
+//! \date       2018 Jan 16
+//! \brief      iris resource archive.
+//
+// Copyright (C) 2018 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+
+// [000728][iris] 聖白薔薇学園プチ☆MAHJONG
+
+namespace GameRes.Formats.Iris
+{
+    [Export(typeof(ArchiveFormat))]
+    public class DatOpener : ArchiveFormat
+    {
+        public override string         Tag { get { return "DAT/FPACK"; } }
+        public override string Description { get { return "Iris resource archive"; } }
+        public override uint     Signature { get { return 0x43415046; } } // 'FPACK'
+        public override bool  IsHierarchic { get { return false; } }
+        public override bool      CanWrite { get { return false; } }
+
+        public override ArcFile TryOpen (ArcView file)
+        {
+            if (!file.View.AsciiEqual (4, "K\0"))
+                return null;
+            if (file.View.ReadUInt32 (8) != file.MaxOffset)
+                return null;
+            int count = file.View.ReadInt16 (6);
+            if (!IsSaneCount (count))
+                return null;
+
+            uint index_offset = 0x10;
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
+            {
+                var name = file.View.ReadString (index_offset, 0x10);
+                if (string.IsNullOrEmpty (name))
+                    return null;
+                var entry = FormatCatalog.Instance.Create<Entry> (name);
+                entry.Offset = file.View.ReadUInt32 (index_offset+0x10);
+                entry.Size   = file.View.ReadUInt32 (index_offset+0x14);
+                if (!entry.CheckPlacement (file.MaxOffset))
+                    return null;
+                dir.Add (entry);
+                index_offset += 0x18;
+            }
+            return new ArcFile (file, this, dir);
+        }
+    }
+}
