ba8d6648bf7560af65c8cea057b5576a81a83dcc
morkt <morkt@users.noreply.github.com>
2018-05-27 15:55:00 +0400

    (Legacy): IFP archives.

--- Legacy/Winters/ArcIFP.cs
+++ Legacy/Winters/ArcIFP.cs
@@ -1,5 +1,5 @@
-//! \file       ArcIFX.cs
-//! \date       2018 Mar 06
+//! \file       ArcIFP.cs
+//! \date       2018 May 23
 //! \brief      Winters resource archive.
 //
 // Copyright (C) 2018 by morkt
@@ -28,36 +28,61 @@ using System.ComponentModel.Composition;
 using System.IO;
 
 // [020426][Winters] Kiss x 200 To Aru Bunkou no Hanashi
+// [040528][Winters] Kiss x 300 Konna Sekai
 
 namespace GameRes.Formats.Winters
 {
     [Export(typeof(ArchiveFormat))]
-    public class IfxOpener : ArchiveFormat
+    public class IfpOpener : ArchiveFormat
     {
-        public override string         Tag { get { return "IFX"; } }
+        public override string         Tag { get { return "IFP"; } }
         public override string Description { get { return "Winters resource archive"; } }
-        public override uint     Signature { get { return 0x65; } }
+        public override uint     Signature { get { return 0x53474149; } } // 'IAGS_IFP_01'
         public override bool  IsHierarchic { get { return false; } }
         public override bool      CanWrite { get { return false; } }
 
         public override ArcFile TryOpen (ArcView file)
         {
-            if (!file.Name.HasExtension (".ifx") || file.MaxOffset <= 0x10000)
+            if (!file.View.AsciiEqual (4, "_IFP_01     ")
+                || file.View.ReadInt32 (0x10) != 1)
+                return null;
+            int count = file.View.ReadInt32 (0x18) / 0x10 - 1;
+            if (!IsSaneCount (count))
                 return null;
             var base_name = Path.GetFileNameWithoutExtension (file.Name);
             var dir = new List<Entry>();
-            for (int i = 0, index_offset = 0x20; index_offset < 0x10000; index_offset += 0x10, ++i)
+            for (int i = 0, index_offset = 0x20; i < count; ++i, index_offset += 0x10)
             {
-                if (file.View.ReadUInt16 (index_offset) == 0)
+                int type = file.View.ReadUInt16 (index_offset);
+                if (0 == type)
                     continue;
-                var name = string.Format ("{0}#{1:D5}", base_name, i);
-                uint offset = file.View.ReadUInt32 (index_offset+4);
-                var entry = AutoEntry.Create (file, offset, name);
-                entry.Size = file.View.ReadUInt32 (index_offset+8);
+                int mask_type = file.View.ReadUInt16 (index_offset+2);
+                var entry = new Entry {
+                    Name = string.Format ("{0}#{1:D5}", base_name, i),
+                    Offset = file.View.ReadUInt32 (index_offset+4),
+                    Size = file.View.ReadUInt32 (index_offset+8),
+                };
                 if (!entry.CheckPlacement (file.MaxOffset))
                     return null;
+                switch (type)
+                {
+                case 0x0B: entry.ChangeType (ImageFormat.Bmp); break;
+                case 0x0C: entry.Type = "image"; break;
+                case 0x0D: entry.ChangeType (ImageFormat.Jpeg); break;
+                case 0x15: entry.Type = "script"; break;
+                }
                 dir.Add (entry);
-                index_offset += 0x10;
+                uint mask_size = file.View.ReadUInt32 (index_offset+12);
+                if (0x0B == mask_type && 0 != mask_size)
+                {
+                    entry = new Entry {
+                        Name = string.Format ("{0}#{1:D5}M.bmp", base_name, i),
+                        Type = "image",
+                        Offset = entry.Offset + entry.Size,
+                        Size = mask_size,
+                    };
+                    dir.Add (entry);
+                }
             }
             if (0 == dir.Count)
                 return null;
