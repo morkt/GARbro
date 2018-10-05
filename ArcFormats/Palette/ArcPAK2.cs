b76aa6588f06433fc5681b70e795e952b62c57d3
morkt <morkt@users.noreply.github.com>
2018-10-06 02:56:46 +0400

    (Pak2Opener): moved to separate file.

--- ArcFormats/Palette/ArcPAK2.cs
+++ ArcFormats/Palette/ArcPAK2.cs
@@ -1,6 +1,7 @@
-//! \file       ArcPAK.cs
+//! \file       ArcPAK2.cs
 //! \date       Mon Oct 26 04:40:25 2015
 //! \brief      Palette PACK2 resource archives.
+//! \brief      
 //
 // Copyright (C) 2015-2018 by morkt
 //
@@ -25,50 +26,52 @@
 
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using GameRes.Utility;
 
 namespace GameRes.Formats.Palette
 {
     [Export(typeof(ArchiveFormat))]
-    public class FilePackOpener : ArchiveFormat
+    public class Pak2Opener : ArchiveFormat
     {
-        public override string         Tag { get { return "PAK/FilePack"; } }
+        public override string         Tag { get { return "PACK2/Palette"; } }
         public override string Description { get { return "Palette resource archive"; } }
-        public override uint     Signature { get { return 0x656C6946; } } // 'FilePack'
-        public override bool  IsHierarchic { get { return false; } }
+        public override uint     Signature { get { return 0x43415005; } } // '\x05PACK2'
+        public override bool  IsHierarchic { get { return true; } }
         public override bool      CanWrite { get { return false; } }
 
-        public FilePackOpener ()
+        public Pak2Opener ()
         {
+            Extensions = new string[] { "pak" };
             ContainedFormats = new[] { "PGA", "CHR/Palette", "OGG", "WAV" };
         }
 
         public override ArcFile TryOpen (ArcView file)
         {
-            if (!file.View.AsciiEqual (4, "Pack"))
+            if (!file.View.AsciiEqual (4, "K2"))
                 return null;
-            int count = file.View.ReadInt32 (8);
+            int count = file.View.ReadInt32 (6);
             if (!IsSaneCount (count))
                 return null;
 
-            uint index_offset = 0xC;
-            var name_buf = new byte[0x20];
+            uint index_offset = 0xA;
+            var name_buf = new byte[0x40];
             var dir = new List<Entry> (count);
             for (int i = 0; i < count; ++i)
             {
-                file.View.Read (index_offset, name_buf, 0, 0x20);
-                for (int j = 0; j < 0x20; ++j)
+                int name_length = file.View.ReadByte (index_offset++);
+                if (name_length > name_buf.Length)
+                    name_buf = new byte[name_length];
+                file.View.Read (index_offset, name_buf, 0, (uint)name_length);
+                for (int j = 0; j < name_length; ++j)
                     name_buf[j] ^= 0xFF;
-                var name = Binary.GetCString (name_buf, 0);
-                if (string.IsNullOrEmpty (name))
-                    return null;
+                index_offset += (uint)name_length;
+                var name = Encodings.cp932.GetString (name_buf, 0, name_length);
                 var entry = Create<Entry> (name);
-                entry.Size   = file.View.ReadUInt32 (index_offset+0x20);
-                entry.Offset = file.View.ReadUInt32 (index_offset+0x24);
+                entry.Offset = file.View.ReadUInt32 (index_offset);
+                entry.Size   = file.View.ReadUInt32 (index_offset+4);
                 if (!entry.CheckPlacement (file.MaxOffset))
                     return null;
                 dir.Add (entry);
-                index_offset += 0x28;
+                index_offset += 8;
             }
             return new ArcFile (file, this, dir);
         }
