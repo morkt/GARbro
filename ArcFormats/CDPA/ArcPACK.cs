4acc277c407c150329d1c28e83df0a0eafa05589
morkt <morkt@users.noreply.github.com>
2018-10-27 09:59:18 +0400

    implemented CDPA 'PACK' archives.

---
+++ ArcFormats/CDPA/ArcPACK.cs
@@ -0,0 +1,69 @@
+//! \file       ArcPACK.cs
+//! \date       2018 Oct 25
+//! \brief      CDPA resource archive.
+//
+// Copyright (C) 2018 by morkt
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace GameRes.Formats.Cdpa
+{
+    [Export(typeof(ArchiveFormat))]
+    public class PackOpener : ArchiveFormat
+    {
+        public override string         Tag { get { return "PACK/CDPA"; } }
+        public override string Description { get { return "CDPA resource archive"; } }
+        public override uint     Signature { get { return 0x4B434150; } } // 'PACK'
+        public override bool  IsHierarchic { get { return false; } }
+        public override bool      CanWrite { get { return false; } }
+
+        public PackOpener ()
+        {
+            Extensions = new string[] { "" };
+        }
+
+        public override ArcFile TryOpen (ArcView file)
+        {
+            int count = file.View.ReadInt32 (4);
+            if (!IsSaneCount (count))
+                return null;
+            uint index_offset = 8;
+            uint data_offset = index_offset + 0x28 * (uint)count;
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
+            {
+                var name = file.View.ReadString (index_offset, 0x20);
+                if (string.IsNullOrWhiteSpace (name))
+                    return null;
+                var entry = FormatCatalog.Instance.Create<Entry> (name);
+                entry.Size = file.View.ReadUInt32 (index_offset+0x20);
+                entry.Offset = file.View.ReadUInt32 (index_offset+0x24);
+                if (entry.Offset < data_offset || !entry.CheckPlacement (file.MaxOffset))
+                    return null;
+                dir.Add (entry);
+                index_offset += 0x28;
+            }
+            return new ArcFile (file, this, dir);
+        }
+    }
+}
