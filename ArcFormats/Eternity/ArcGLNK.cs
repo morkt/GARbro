d45303945faeef5c2d9fcafbf1a2f6c500c2d356
morkt <morkt@users.noreply.github.com>
2018-11-10 22:21:45 +0400

    (GLNK): moved to separate file.

--- ArcFormats/Eternity/ArcGLNK.cs
+++ ArcFormats/Eternity/ArcGLNK.cs
@@ -1,8 +1,8 @@
-//! \file       ArcMiris.cs
-//! \date       Thu Sep 03 13:12:53 2015
-//! \brief      Studio Miris archives implementation.
+//! \file       ArcGLNK.cs
+//! \date       2018 Nov 10
+//! \brief      Studio Miris archive implementation.
 //
-// Copyright (C) 2015 by morkt
+// Copyright (C) 2015-2018 by morkt
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
@@ -23,64 +23,54 @@
 // IN THE SOFTWARE.
 //
 
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.IO;
-using System.Text.RegularExpressions;
-using GameRes.Compression;
 
 namespace GameRes.Formats.Miris
 {
     [Export(typeof(ArchiveFormat))]
-    public class DatOpener : ArchiveFormat
+    public class GlnkOpener : ArchiveFormat
     {
-        public override string         Tag { get { return "DAT/MIRIS"; } }
+        public override string         Tag { get { return "GLNK/MIRIS"; } }
         public override string Description { get { return "Studio Miris resource archive"; } }
-        public override uint     Signature { get { return 0; } }
+        public override uint     Signature { get { return 0x4B4E4C47; } } // 'GLNK'
         public override bool  IsHierarchic { get { return false; } }
         public override bool      CanWrite { get { return false; } }
 
-        public DatOpener ()
+        public GlnkOpener ()
         {
-            Extensions = new string[] { "dat" };
+            Extensions = new string[] { "dat", "glk", "mlk", "slk", "gl", "ml", "sl", "ets", "etg", "etm" };
         }
-
-        static readonly Regex IndexEntryRe = new Regex (@"\G([^,]+),(\d+),(\d+)#");
 
         public override ArcFile TryOpen (ArcView file)
         {
-            var base_dir = VFS.GetDirectoryName (file.Name);
-            var base_name = Path.GetFileNameWithoutExtension (file.Name);
-            var list_file = VFS.CombinePath (base_dir, base_name+"l.dat");
-            if (!VFS.FileExists (list_file))
+            int version = file.View.ReadUInt16 (4);
+            int count = file.View.ReadInt32 (6);
+            if (!IsSaneCount (count))
                 return null;
-            string index;
-            using (var ls = VFS.OpenStream (list_file))
-            using (var zls = new ZLibStream (ls, CompressionMode.Decompress))
-            using (var reader = new StreamReader (zls, Encodings.cp932))
-            {
-                index = reader.ReadToEnd();
-            }
-            if (string.IsNullOrEmpty (index))
+            long index_offset = file.View.ReadUInt32 (0xA);
+            int index_length = file.View.ReadInt32 (0xE);
+            if (index_length > file.View.Reserve (index_offset, (uint)index_length))
                 return null;
+            int entry_size = version >= 0x6E ? 12 : 8;
 
-            var dir = new List<Entry>();
-            var match = IndexEntryRe.Match (index);
-            while (match.Success)
+            var dir = new List<Entry> (count);
+            for (int i = 0; i < count; ++i)
             {
-                var entry = new Entry {
-                    Name    = match.Groups[1].Value,
-                    Offset  = UInt32.Parse (match.Groups[3].Value),
-                    Size    = UInt32.Parse (match.Groups[2].Value),
-                };
+                if (index_length <= 0)
+                    return null;
+                int name_length = file.View.ReadByte (index_offset++);
+                string name = file.View.ReadString (index_offset, (uint)name_length);
+                index_offset += name_length;
+                var entry = Create<Entry> (name);
+                entry.Offset = file.View.ReadUInt32 (index_offset);
+                entry.Size = file.View.ReadUInt32 (index_offset+4);
                 if (!entry.CheckPlacement (file.MaxOffset))
                     return null;
                 dir.Add (entry);
-                match = match.NextMatch();
+                index_offset += entry_size;
+                index_length -= 1+name_length + entry_size;
             }
-            if (0 == dir.Count)
-                return null;
             return new ArcFile (file, this, dir);
         }
     }
