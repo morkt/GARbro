492f37b82b7d0d96f6e7c943995ca3af2337f86c
Crsky <crskycode@hotmail.com>
2022-11-29 21:50:14 +0800

    Fix project files

--- Experimental/Properties/AssemblyInfo.cs
+++ Experimental/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.16.40")]
-[assembly: AssemblyFileVersion ("1.0.16.40")]
+[assembly: AssemblyVersion ("1.0.16.46")]
+[assembly: AssemblyFileVersion ("1.0.16.46")]
